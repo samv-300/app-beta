ff49746a61feb8362e197652c52ef33a16ceb6e3
Sam <samuelportillovillota@gmail.com>
2026-05-13 08:08:25 -0500

    m

--- src/signup.php
+++ src/signup.php
@@ -1,4 +1,5 @@
 <?php 
+
 //CONECTARSE A LA BASE DE DATOS 
 include('../config/database.php');
 
@@ -7,11 +8,11 @@ $f_name     =   $_POST['fname' ]  ;
 $l_name     =   $_POST['lname' ]  ;
 $e_mail     =   $_POST['email' ]  ;
 $m_phone    =   $_POST['mphone']  ;
-$p_sswd     =   $_POST['passwd']  ;
+$p_sswd     =   $_POST['psswd']  ;
 $enc_pass   = md5($p_sswd);
 
 //query to insert into SQL
-$sql ="INSERT INTO users(firstname, lastname, email, mphone, password) 
+$sql ="INSERT INTO users(firstname, lastname, email, mobile_phone, password) 
     values('$f_name', '$l_name', '$e_mail','$m_phone','$enc_pass')";
 //execute query
 pg_query($sql);
